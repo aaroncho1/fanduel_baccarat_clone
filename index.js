00bee8b806343d0bda35de34bbafab9cb9b0a6f9
Aaron <aaroncho10@gmail.com>
2024-02-23 23:11:07 -0500

    added html for footer and added js code for date and time

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-let nav = document.getElementById("navbar");
-let sticky = nav.offsetTop;
-window.onscroll = () => {
-    sticker()
-};
-let sticker = () => {
-    if (window.pageYOffset >= sticky) {
-        nav.classList.add("sticky")
-    } else {
-        nav.classList.remove("sticky")
-    }
-}
+var now = new Date();
+var datetime = now.toLocaleString();
+
+document.getElementById("datetime").innerHTML = datetime;
+
+
+
