dbbe772b1fcc25973ee6405281f4bb9447d9a6fb
Aaron <aaroncho10@gmail.com>
2024-03-03 01:30:54 -0500

    added javascript for next scroll button in header nav

--- index.js
+++ index.js
@@ -3,5 +3,24 @@ var datetime = now.toLocaleString();
 
 document.getElementById("datetime").innerHTML = datetime;
 
+let button = document.querySelector('.arrow-next')
+button.onclick = function(){
+    let container = document.querySelector('.nav-container');
+    sideScroll(container, 'right', 15, 250, 10);
+}
 
+function sideScroll(element, direction, speed, distance, step){
+    scrollAmount = 0;
+    let slideTimer = setInterval(function(){
+        if(direction == 'left'){
+            element.scrollLeft -= step;
+        } else {
+            element.scrollLeft += step;
+        }
+        scrollAmount += step;
+        if(scrollAmount >= distance){
+            window.clearInterval(slideTimer);
+        }
+    }, speed);
+}
 
